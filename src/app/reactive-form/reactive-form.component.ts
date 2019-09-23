e068c17e56ca05a3768ca9b3ee403f5572ad9ee5
K0H205 <hrswkhd@gmail.com>
2019-09-23 21:12:17 +0900

    fix

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,39 +1,31 @@
-import { Component, OnInit } from "@angular/core";
-import {
-  FormBuilder,
-  FormGroup,
-  FormControl,
-  Validators,
-  AbstractControl,
-  FormArray
-} from "@angular/forms";
-import { identityRevealedValidator } from "./passwordMatch.directive";
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, FormArray } from '@angular/forms';
 
 interface Item {
   name: string;
   price: string;
 }
 @Component({
-  selector: "app-reactive-form",
-  templateUrl: "./reactive-form.component.html",
-  styleUrls: ["./reactive-form.component.scss"]
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.scss']
 })
 export class ReactiveFormComponent implements OnInit {
   musicGenre = [
-    { id: 1, genre: "Pop" },
-    { id: 2, genre: "Rock" },
-    { id: 3, genre: "Techno" },
-    { id: 4, genre: "Hiphop" }
+    { id: 1, genre: 'Pop' },
+    { id: 2, genre: 'Rock' },
+    { id: 3, genre: 'Techno' },
+    { id: 4, genre: 'Hiphop' }
   ];
   formControls = this.musicGenre.map(val => new FormControl());
-  ngForm = this.fb.group({
-    name: [""],
-    price: [""],
+  ngForm: FormGroup = this.fb.group({
+    name: [''],
+    price: [''],
     musicPreferences: new FormArray(this.formControls)
   });
 
   get musicPreferences() {
-    const values: string[] = this.ngForm.get("musicPreferences").value;
+    const values: string[] = this.ngForm.get('musicPreferences').value;
     return values
       .map((value, index) => {
         return value ? this.musicGenre[index].genre : null;
