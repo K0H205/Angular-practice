38f670815a5e07b79c7fb1390f85eb256b0f8b9d
K0H205 <hrswkhd@gmail.com>
2019-09-23 21:09:29 +0900

    create FormArray getter

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,64 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import {
   FormBuilder,
   FormGroup,
   FormControl,
   Validators,
-  AbstractControl
-} from '@angular/forms';
-import { identityRevealedValidator } from './passwordMatch.directive';
+  AbstractControl,
+  FormArray
+} from "@angular/forms";
+import { identityRevealedValidator } from "./passwordMatch.directive";
 
 interface Item {
   name: string;
   price: string;
 }
 @Component({
-  selector: 'app-reactive-form',
-  templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.scss']
+  selector: "app-reactive-form",
+  templateUrl: "./reactive-form.component.html",
+  styleUrls: ["./reactive-form.component.scss"]
 })
 export class ReactiveFormComponent implements OnInit {
-  constructor(private fb: FormBuilder) {}
-  items = [];
-  checkoutForm: FormGroup;
-  name = new FormControl('hoge');
-  passwordForm = this.fb.group({
-    password: [
-      '',
-      Validators.compose([Validators.required, Validators.minLength(6)])
-    ],
-    confirmPassword: ['', Validators.required]
+  musicGenre = [
+    { id: 1, genre: "Pop" },
+    { id: 2, genre: "Rock" },
+    { id: 3, genre: "Techno" },
+    { id: 4, genre: "Hiphop" }
+  ];
+  formControls = this.musicGenre.map(val => new FormControl());
+  ngForm = this.fb.group({
+    name: [""],
+    price: [""],
+    musicPreferences: new FormArray(this.formControls)
   });
 
-  passwordError = false;
+  get musicPreferences() {
+    const values: string[] = this.ngForm.get("musicPreferences").value;
+    return values
+      .map((value, index) => {
+        return value ? this.musicGenre[index].genre : null;
+      })
+      .filter(Boolean);
+  }
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.checkoutForm = this.fb.group({
-      name: '',
-      price: ''
-    });
+    this.ngForm.valueChanges.subscribe(val =>
+      console.log(this.musicPreferences)
+    );
   }
 
-  onSubmit(formData: Item) {
-    this.items.push(formData);
-    // TODO: Use EventEmitter with form value
-    console.warn(this.items);
-
-    this.checkoutForm.reset();
-  }
-
-  onSubmit2() {
-    const password = this.passwordForm.get('password').value;
-    const confirmPassword = this.passwordForm.get('confirmPassword').value;
-    if (password !== confirmPassword) {
-      this.passwordError = true;
-    } else {
-      this.passwordError = false;
-    }
-    console.log(password.value);
-    // TODO: Use EventEmitter with form value
-    console.warn(this.items);
-
-    this.checkoutForm.reset();
+  onSubmit() {
+    console.log(this.musicPreferences);
+    console.log(this.ngForm);
   }
 }
